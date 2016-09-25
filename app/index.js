5df4dc45add14a712d9ff929d6434e232560ea69
edloidas <edloidas@gmail.com>
2016-09-26 00:43:19 +0300

    Add basic functionality.
    Added constructor and method to make generator executable.

--- app/index.js
+++ app/index.js
@@ -2,4 +2,11 @@
 
 var yeoman = require('yeoman-generator');
 
-module.exports = yeoman.Base.extend();
+module.exports = yeoman.Base.extend({
+  constructor: function constructor() {
+    yeoman.Base.apply(this, arguments);
+  },
+  dummy: function dummy() {
+    console.log('Dummy method ran.');
+  },
+});
