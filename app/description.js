05a46c259b3d0d1791c8656601fa8bac92e40e7d
edloidas <edloidas@gmail.com>
2016-10-01 00:42:30 +0300

    Add ESLint config
    Added ESLint config.
    Added `--debug` option.

---
+++ app/description.js
@@ -0,0 +1,8 @@
+const name = `Project’s name
+\tchange the current working directory to newly created
+\tand initialize project in that directory
+    `;
+
+module.exports = {
+  name,
+};
--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
-'use strict';
-
-var yeoman = require('yeoman-generator');
+const yeoman = require( 'yeoman-generator' );
+const description = require( './description' );
 
 module.exports = yeoman.Base.extend({
-  constructor: function constructor() {
-    yeoman.Base.apply(this, arguments);
+  constructor: function constructor( ...args ) {
+    yeoman.Base.apply( this, args );
+    this.argument( 'name', {
+      type: String, required: false,
+      desc: description.name,
+    });
+    this.option( 'debug', {
+      type: Boolean, required: false, alias: 'd', defaults: false,
+      desc: 'Debug mode',
+    });
   },
-  dummy: function dummy() {
-    console.log('Dummy method ran.');
+  initialize: function initialize() {
+    if ( this.options.debug ) {
+      this.log( `ARGUMENT: name ${ this.name }` );
+    }
   },
 });
